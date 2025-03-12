f204920f4c1908ef18cf32ad0d4ddeaa3f6ab2ce
PANARIN <panarin@PANARIN.panarin>
2025-03-13 00:19:16 +0700

    some fixes

--- src/main/java/org/cwt/task/model/UserAnalytic.java
+++ src/main/java/org/cwt/task/model/UserAnalytic.java
@@ -15,11 +15,19 @@ public class UserAnalytic {
     String firstName;
     String lastName;
 
+    // Оборот книг у пользователя
     Integer countRent;
+
+    // Количество выданных, но на момент временного интервала не возвращенных книг
     Integer countOpenRent;
+
+    // Количество возвращенных книг на временном интервале
     Integer countCloseRent;
 
+    // Выданные книги на временном интервале
     List<String> openBook;
+
+    // Возвращенные книги на временном интервале
     List<String> closeBook;
 
 }
